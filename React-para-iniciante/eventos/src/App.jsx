3ce720967f2ce7e977c8f828d5e3bddb30fca94d
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-03-25 11:58:13 -0300

    inicio das aulas de evento em react

---
+++ React-para-iniciante/eventos/src/App.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+// Organize os produtos como mostrado no vídeo
+// Mostre apenas produtos que forem mais caros que R$ 1500
+
+const produtos = [
+  {
+    id: 1,
+    nome: "Smartphone",
+    preco: "R$ 2000",
+    cores: ["#29d8d5", "#252a34", "#fc3766"],
+  },
+  {
+    id: 2,
+    nome: "Notebook",
+    preco: "R$ 3000",
+    cores: ["#ffd045", "#d4394b", "#f37c59"],
+  },
+  {
+    id: 3,
+    nome: "Tablet",
+    preco: "R$ 1500",
+    cores: ["#365069", "#47c1c8", "#f95786"],
+  },
+];
+
+const App = () => {
+  return (
+    <section>
+      {produtos
+        .filter((produto) => Number(produto.preco.replace("R$ ", " ")) > 1500)
+        .map((produto) => (
+          <div key={produto.id}>
+            <h1>{produto.nome}</h1>
+            <p>Preço: {produto.preco}</p>
+            <ul>
+              {produto.cores.map((cor) => (
+                <li key={cor} style={{ background: cor, color: "white" }}>
+                  {cor}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+    </section>
+  );
+};
+
+export default App;
