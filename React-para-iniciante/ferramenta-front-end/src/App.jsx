b56073aae0d8b71f71c280d8c186a1d5773a7cf5
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-03-25 11:08:13 -0300

    terminada a aula sobre jsx com arrays

--- React-para-iniciante/ferramenta-front-end/src/App.jsx
+++ React-para-iniciante/ferramenta-front-end/src/App.jsx
@@ -1,49 +1,26 @@
-import React from "react";
-
-const luana = {
-  cliente: "Luana",
-  idade: 27,
-  compras: [
-    { nome: "Notebook", preco: "R$ 2500" },
-    { nome: "Geladeira", preco: "R$ 3000" },
-    { nome: "Smartphone", preco: "R$ 1500" },
-  ],
-  ativa: true,
-};
-
-const mario = {
-  cliente: "Mario",
-  idade: 31,
-  compras: [
-    { nome: "Notebook", preco: "R$ 2500" },
-    { nome: "Geladeira", preco: "R$ 3000" },
-    { nome: "Smartphone", preco: "R$ 1500" },
-    { nome: "Guitarra", preco: "R$ 3500" },
-  ],
-  ativa: false,
-};
+// Organize os produtos como mostrado no vídeo
+// Mostre apenas produtos que forem mais caros que R$ 1500
+const produtos = [
+  {
+    id: 1,
+    nome: "Smartphone",
+    preco: "R$ 2000",
+    cores: ["#29d8d5", "#252a34", "#fc3766"],
+  },
+  {
+    id: 2,
+    nome: "Notebook",
+    preco: "R$ 3000",
+    cores: ["#ffd045", "#d4394b", "#f37c59"],
+  },
+  {
+    id: 3,
+    nome: "Tablet",
+    preco: "R$ 1500",
+    cores: ["#365069", "#47c1c8", "#f95786"],
+  },
+];
 
 const App = () => {
-  const dados = mario;
-
-  const total = dados.compras
-    .map((item) => Number(item.preco.replace("R$ ", "")))
-    .reduce((a, b) => a + b);
-
-  return (
-    <>
-      <p>Nome: {dados.cliente}</p>
-      <p>Idade: {dados.idade}</p>
-      <p>
-        Situação:{" "}
-        <span style={dados.ativa ? { color: "green" } : { color: "red" }}>
-          {dados.ativa ? "Ativa" : "Inativa"}
-        </span>
-      </p>
-      <p>Total Gasto R${total}</p>
-      {total > 10000 && <p>Você está gastando muito.</p>}
-    </>
-  );
+  return <section></section>;
 };
-
-export default App;
