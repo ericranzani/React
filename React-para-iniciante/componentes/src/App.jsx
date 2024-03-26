58cd80c806278ad32c60696a9c665842a715680d
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-03-26 11:58:51 -0300

    explicação sobre react e a sua funcionalidade

--- React-para-iniciante/componentes/src/App.jsx
+++ React-para-iniciante/componentes/src/App.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Form from "./Form/Form";
+
+const Teste = () => {
+  const active = false;
+  if (active) {
+    return <p>Teste</p>;
+  } else {
+    return null;
+  }
+};
 
 const App = () => {
-  function handleScroll(event) {
-    console.log(event);
-  }
-
-  window.addEventListener("scroll", handleScroll);
-
   return (
-    <div style={{ height: "200vh" }}>
-      <button onClick={(event) => alert(event.target.innerText)}>Click</button>
-    </div>
+    <React.Fragment>
+      <Teste />
+      <Header />
+      <Form />
+      <Footer />
+    </React.Fragment>
   );
 };
 
