bc8e6480b6feae692b12729345b8e8e5791e8476
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-03-26 11:38:30 -0300

    inicio das aulas de componente

---
+++ React-para-iniciante/componentes/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  function handleScroll(event) {
+    console.log(event);
+  }
+
+  window.addEventListener("scroll", handleScroll);
+
+  return (
+    <div style={{ height: "200vh" }}>
+      <button onClick={(event) => alert(event.target.innerText)}>Click</button>
+    </div>
+  );
+};
+
+export default App;
