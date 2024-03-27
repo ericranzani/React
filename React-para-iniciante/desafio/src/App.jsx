50d685c080c299fe4312b7a8f8e3700591c6c895
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-03-27 13:28:26 -0300

    feito o componente home

--- React-para-iniciante/desafio/src/App.jsx
+++ React-para-iniciante/desafio/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import Home from "./Home";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Home />
     </>
   );
 };
---
+++ React-para-iniciante/desafio/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 style={{ color: "green" }}>Home</h1>
+      <p>Essa é a home do site</p>
+    </div>
+  );
+};
+
+export default Home;
