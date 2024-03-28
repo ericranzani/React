547a4017b0cf2359cba50764a98257ec4520d95f
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-03-28 12:11:10 -0300

    configurações

--- React-hooks/useState/src/App.jsx
+++ React-hooks/useState/src/App.jsx
@@ -1,24 +1,7 @@
 import React from "react";
-import Header from "./Header";
-import Home from "./Home";
-import Produtos from "./Produtos";
 
 const App = () => {
-  const { pathname } = window.location;
-
-  let Pagina;
-  if (pathname === "/produtos") {
-    Pagina = Produtos;
-  } else {
-    Pagina = Home;
-  }
-
-  return (
-    <section>
-      <Header />
-      <Pagina />
-    </section>
-  );
+  return <div></div>;
 };
 
 export default App;
--- React-hooks/useState/src/Titulo.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Titulo = (props) => {
-  return <h1 style={{ color: "green" }}>{props.texto}</h1>;
-};
-
-export default Titulo;
