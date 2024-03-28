fed7425e605fec3983fb070cfbdb09c635b76099
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-03-28 11:50:21 -0300

    preparação para o modulo de hooks

---
+++ React-hooks/useState/src/Produtosmeu.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Titulo from "./Titulo";
+
+// Replique a interface como a apresentada na aula
+// Utilize a array abaixo para mostrar os produtos
+// Quebre em componentes o que precisar ser reutilizado
+// Dica: const { pathname } = window.location; (puxa o caminho do URL)
+
+const Produtosmeu = () => {
+  const produtos = [
+    { nome: "Notebook", propriedades: ["16gb ram", "512gb"] },
+    { nome: "Smartphone", propriedades: ["2gb ram", "128gb"] },
+  ];
+
+  return (
+    <section>
+      <Titulo texto="Produtos" />
+      {produtos.map((produto) => (
+        <div style={{ border: "1px solid", margin: "1rem 0", padding: "1rem" }}>
+          <p>{produto.nome}</p>
+          <ul>
+            {produto.propriedades.map((propriedade) => (
+              <li key={propriedade}>{propriedade}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default Produtosmeu;
