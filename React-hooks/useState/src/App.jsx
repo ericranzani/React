c274c499026f8340ba767fbef380023a78713ba0
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-03-28 12:48:40 -0300

    aula de hook, foi explicando utilizando botão de abrir e fechar com useState

--- React-hooks/useState/src/App.jsx
+++ React-hooks/useState/src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import ButtonModal from "./ButtonModal";
+import Modal from "./Modal";
 
 const App = () => {
-  return <div></div>;
+  const [modal, setModal] = React.useState(false);
+
+  return (
+    <div>
+      <Modal modal={modal} setModal={setModal} />
+      <ButtonModal setModal={setModal} />
+    </div>
+  );
 };
 
 export default App;
