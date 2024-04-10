0eab0f5f7a7aefd2996cb8b6a0a7ddd19bb6bdc6
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-04-10 10:42:02 -0300

    arrumando o ambiente

--- React-hooks/useEffect/src/App.jsx
+++ React-hooks/useEffect/src/App.jsx
@@ -1,34 +1,7 @@
 import React from "react";
-// aula 2 useState
+
 const App = () => {
-  const [contar, setContar] = React.useState(1);
-  const [items, setItems] = React.useState(["Item 1"]);
-
-  function handleClick() {
-    // separando as funções deixa mais clean
-    // e não ocorre nenhum efeito colateral
-    setContar(contar + 1);
-    setItems([...items, "Item" + (contar + 1)]);
-  }
-
-  /*   function handleClick() {
-    // Caso estiver usando o StrictMode, 
-    // essa função vai ser renderizada duas vezez
-    // na qual vai começar gerar erro
-    setContar((contar) => {
-      setItems((items) => [...items, "Item" + (contar + 1)]);
-      return contar + 1;
-    });
-  } */
-
-  return (
-    <div>
-      {items.map((item) => (
-        <li key={item}>{item}</li>
-      ))}
-      <button onClick={handleClick}>{contar}</button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
