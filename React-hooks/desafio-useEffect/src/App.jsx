8d64e88231124fc7987b00547959358763b5df8f
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-04-10 12:06:35 -0300

    tarminada a requisição da API junto com a funcionalidades simples dos botão para trazer os dados

--- React-hooks/desafio-useEffect/src/App.jsx
+++ React-hooks/desafio-useEffect/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Produto from "./Produto";
 // Quando o usuário clicar em um dos botões, faça um fetch do produto clicado utilizando a api abaixo
 // https://ranekapi.origamid.dev/json/api/produto/notebook
 // https://ranekapi.origamid.dev/json/api/produto/smartphone
@@ -7,7 +8,28 @@ import React from "react";
 // Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
 
 const App = () => {
-  return <div></div>;
+  const [dados, setDados] = React.useState(null);
+
+  async function handleClick(event) {
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
+    );
+    const json = await response.json();
+    setDados(json);
+  }
+
+  return (
+    <div>
+      <h1>Preferências:</h1>
+      <button style={{ margin: "0.5rem" }} onClick={handleClick}>
+        notebook
+      </button>
+      <button style={{ margin: "0.5rem" }} onClick={handleClick}>
+        smartphone
+      </button>
+      <Produto dados={dados} />
+    </div>
+  );
 };
 
 export default App;
