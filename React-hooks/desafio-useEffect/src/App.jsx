e7cdb02f2fb30afd95144854c3f5a2d3d8550fff
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-04-10 11:42:34 -0300

    anotação do desafio

--- React-hooks/desafio-useEffect/src/App.jsx
+++ React-hooks/desafio-useEffect/src/App.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+// Quando o usuário clicar em um dos botões, faça um fetch do produto clicado utilizando a api abaixo
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+// https://ranekapi.origamid.dev/json/api/produto/smartphone
+// Mostre o nome e preço na tela (separe essa informação em um componente Produto.js)
+// Defina o produto clicado como uma preferência do usuário no localStorage
+// Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
 
 const App = () => {
   return <div></div>;
