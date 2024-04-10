d8280fea4bc3c74c6dbba4b0cf7b5a139d20fddc
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-04-10 11:35:41 -0300

    aula com exemplos de useEffect

--- React-hooks/useEffect/src/App.jsx
+++ React-hooks/useEffect/src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Produto from "./Produto";
 
 const App = () => {
-  return <div></div>;
+  const [ativo, setAtivo] = React.useState(false);
+
+  return (
+    <div>
+      {ativo && <Produto />}
+      <button onClick={() => setAtivo(!ativo)}>Ativar</button>
+    </div>
+  );
 };
 
 export default App;
