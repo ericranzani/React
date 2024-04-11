7f5fb1312f781cc5bf5975ba91392d25533d215b
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-04-11 11:46:45 -0300

    primeiro exemplo da aula com useRef, criado um aplicativo de input que adiciona itens na lista e mostre na tela

--- React-hooks/useRef/src/App.jsx
+++ React-hooks/useRef/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+// exemplo real
+// uma notificação ao "adicionar item no carrinho"
 
 const App = () => {
-  return <div></div>;
+  const [carrinho, setCarrinho] = React.useState(0);
+
+  function handleClick() {
+    setCarrinho(carrinho + 1);
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>Adicionar Carrinho {carrinho}</button>
+    </div>
+  );
 };
 
 export default App;
