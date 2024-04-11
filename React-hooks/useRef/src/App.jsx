7030738c8cc62b99ac969c4639c23f4a6f51b296
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-04-11 11:56:03 -0300

    exemplo real de notificação, simulando a notificação de quando vc adiciona um item no carrinho em site de compra

--- React-hooks/useRef/src/App.jsx
+++ React-hooks/useRef/src/App.jsx
@@ -4,13 +4,22 @@ import React from "react";
 
 const App = () => {
   const [carrinho, setCarrinho] = React.useState(0);
+  const [notificacao, setNotificacao] = React.useState(null);
+  const timeoutRef = React.useRef();
 
   function handleClick() {
     setCarrinho(carrinho + 1);
+    setNotificacao("Item adicionado ao carrinho!");
+
+    clearTimeout(timeoutRef.current); // limpa o timeout anterior, fazendo que agende um novo
+    timeoutRef.current = setTimeout(() => {
+      setNotificacao(null);
+    }, 2000);
   }
 
   return (
     <div>
+      <p>{notificacao}</p>
       <button onClick={handleClick}>Adicionar Carrinho {carrinho}</button>
     </div>
   );
