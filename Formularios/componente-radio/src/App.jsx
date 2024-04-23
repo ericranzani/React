15b74563c2dc387314b9e1a16bec8f8d30fc6ffc
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-04-23 11:02:57 -0300

    aula de criação do componente em radio de forma reativa

---
+++ Formularios/componente-radio/src/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Input from "./Form/Input";
+import Select from "./Form/Select";
+import Radio from "./Form/Radio";
+
+const App = () => {
+  const [nome, setNome] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [produto, setProduto] = React.useState("");
+  const [cor, setCor] = React.useState("");
+  const [frutas, setFrutas] = React.useState("");
+
+  return (
+    <form>
+      <h2>Cores</h2>
+      <Radio options={["Azul", "Vermelho"]} value={cor} setValue={setCor} />
+
+      <h2>Frutas</h2>
+      <Radio
+        options={["Maçã", "Abacaxi", "Limão"]}
+        value={frutas}
+        setValue={setFrutas}
+      />
+
+      <Select
+        options={["Smartphone", "Tablet"]}
+        value={produto}
+        setValue={setProduto}
+      />
+      <Input id="nome" label="Nome" value={nome} setValue={setNome} />
+      <Input
+        id="email"
+        label="Email"
+        value={email}
+        setValue={setEmail}
+        required
+      />
+      <button>Enviar</button>
+    </form>
+  );
+};
+
+export default App;
