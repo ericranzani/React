db57856e8f43d8b39269972f42f3f737c08c04a4
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-05-02 11:47:14 -0300

    aula sobre css modules, na qual é mais utilizado do que o import, na qual não tem problemas com conflito entre nomes, ja que no modules cria um codigo no nome

---
+++ CSS/css-modules/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Produto from "./Components/Produto";
+
+const App = () => {
+  return (
+    <div>
+      <Produto />
+    </div>
+  );
+};
+
+export default App;
