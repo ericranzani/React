b45cf81a7b8dfa29eebd314d4fd12d97252a15aa
Éric Ranzani <eric_ranzani@yahoo.com.br>
2025-07-28 13:34:22 -0300

    parte 3 do desafios que foi construido a paginas de produtos

--- Desafio/desafio-router/src/components/Produtos.jsx
+++ Desafio/desafio-router/src/components/Produtos.jsx
@@ -1,11 +1,31 @@
 import React from "react";
-import styles from './Produtos.module.css'
+import styles from './Produtos.module.css';
+import { Link } from 'react-router-dom';
+import Head from './Head';
+
 
 const Produtos = () => {
+    const [produtos, setProdutos] = React.useState(null);    
+
+    React.useEffect(() => {
+        fetch('https://ranekapi.origamid.dev/json/api/produto').then(r => r.json())
+        .then(json => setProdutos(json));
+    }, []);
+
+    if(produtos === null) return null;
     return (
-        <div className={styles.produto}>
-            <h1>Produtos</h1>
-        </div>
+        <section className={`${styles.produtos} animeLeft`}>
+            <Head
+                title="Ranek"
+                description="Descrição do site Ranek"
+            />
+            {produtos.map((produto) => (
+                <Link to={`produto/${produto.id}`} key={produto.id}> 
+                    <img src={produto.fotos[0].src} alt={produto.fotos[0].titulo} />
+                    <h1 className={styles.nome}>{produto.id}</h1>
+                </Link>
+            ))}
+        </section>
     )
 }
 
