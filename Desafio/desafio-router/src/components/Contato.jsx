f5b3f5dab22642e70e30d7451717d979a51ea541
Éric Ranzani <eric_ranzani@yahoo.com.br>
2025-07-28 12:49:50 -0300

    desafio parte 2

--- Desafio/desafio-router/src/components/Contato.jsx
+++ Desafio/desafio-router/src/components/Contato.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import styles from './Contato.module.css'
+import foto from '../img/contato.jpg'
+import Head from './Head'
 
 const Contato = () => {
     return (
-        <section className={styles.contato}>
-            <h1>Contato</h1>
+        <section className={`${styles.contato} animeLeft`}>
+            <Head title="Ranek | Contato" description="Entre em contato" />
+            <img src={foto} alt="Maquina de escrever" />
+            <div>
+                <h1>Entre em contato.</h1>
+                <ul className={styles.dados}>
+                    <li>eric@gmail.com</li>
+                    <li>99999-9999</li>
+                    <li>Rua Miro Vetorazzo, 990</li>
+                </ul>
+            </div>   
         </section>
     )
 }
